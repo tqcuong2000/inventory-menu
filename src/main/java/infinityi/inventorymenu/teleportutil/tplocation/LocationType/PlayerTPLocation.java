eb37f5ebca3c3df085d2d5210ef3d5d0556b7f98
cuong <cuong26092@gmail.com>
2025-08-13 19:42:29 +0700

    Added direct_tp field for directly tp to player without sending a request.

--- src/main/java/infinityi/inventorymenu/teleportutil/tplocation/LocationType/PlayerTPLocation.java
+++ src/main/java/infinityi/inventorymenu/teleportutil/tplocation/LocationType/PlayerTPLocation.java
@@ -1,8 +1,11 @@
 package infinityi.inventorymenu.teleportutil.tplocation.LocationType;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import infinityi.inventorymenu.itemaction.actions.TeleportAction;
 import infinityi.inventorymenu.teleportutil.TeleportCost;
+import infinityi.inventorymenu.teleportutil.TeleportUtils;
 import infinityi.inventorymenu.teleportutil.requestmanager.TeleportRequestManager;
 import infinityi.inventorymenu.teleportutil.tplocation.TPLocation;
 import net.minecraft.server.MinecraftServer;
@@ -12,15 +15,31 @@ import net.minecraft.util.math.BlockPos;
 
 import java.util.UUID;
 
-public record PlayerTPLocation(String playerName) implements TPLocation {
+public record PlayerTPLocation(String playerName, Boolean directTP) implements TPLocation {
 
-    public static final Codec<PlayerTPLocation> CODEC = Codec.STRING.xmap(PlayerTPLocation::new, PlayerTPLocation::playerName);
+    public static final Codec<PlayerTPLocation> OBJ_CODEC = RecordCodecBuilder.create(inst -> inst.group(
+            Codec.STRING.fieldOf("name").forGetter(PlayerTPLocation::playerName),
+            Codec.BOOL.optionalFieldOf("direct_tp", true).forGetter(PlayerTPLocation::directTP)
+    ).apply(inst, PlayerTPLocation::new));
+    public static final Codec<PlayerTPLocation> CODEC = Codec.xor(Codec.STRING,OBJ_CODEC).xmap(
+            either -> either.map(l -> new PlayerTPLocation(l, true), r -> r),
+            tp -> {
+                if (tp.directTP) {
+                    return Either.left(tp.playerName);
+                }
+                return Either.right(tp);
+            }
+    );
 
     @Override
     public void teleport(ServerPlayerEntity player, boolean safeCheck, TeleportCost cost) {
         ServerPlayerEntity targetPlayer = getPlayer(player.getServer());
         if (targetPlayer == null) {
             player.sendMessage(Text.translatable("%s §cis not online.", this.playerName));
+            return;
+        }
+        if (directTP) {
+            TeleportUtils.teleport(player, targetPlayer.getPos());
             return;
         }
         TeleportRequestManager.createRequest(player, targetPlayer, safeCheck, cost);
