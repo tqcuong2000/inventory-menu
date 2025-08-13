fa81aa6685095c36e3b805966629d876e0cad8f1
cuong <cuong26092@gmail.com>
2025-08-13 19:52:01 +0700

    Added TeleportUtils.

--- src/main/java/infinityi/inventorymenu/teleportutil/TeleportUtils.java
+++ src/main/java/infinityi/inventorymenu/teleportutil/TeleportUtils.java
@@ -1,15 +1,16 @@
 package infinityi.inventorymenu.teleportutil;
 
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.Collections;
 
 public class TeleportUtils {
 
-    public static void teleport(ServerPlayerEntity player, Vec3d target){
+    public static void teleport(ServerPlayerEntity player, Vec3d target, ServerWorld world){
         if (player.getServer() == null) return;
-        player.getServer().execute(() -> player.teleport(player.getWorld(), target.x , target.y, target.z, Collections.emptySet(), player.headYaw, player.lastPitch, false));
+        player.getServer().execute(() -> player.teleport(world, target.x , target.y, target.z, Collections.emptySet(), player.headYaw, player.lastPitch, false));
     }
 
 }
