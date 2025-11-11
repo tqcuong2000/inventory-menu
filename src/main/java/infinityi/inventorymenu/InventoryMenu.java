9d3a36957428ad1a9caea7a0da7f3c26367ea684
cuong <cuong26092@gmail.com>
2025-11-12 01:33:03 +0700

    make datamanager property public and removed getDatamanager method

--- src/main/java/infinityi/inventorymenu/InventoryMenu.java
+++ src/main/java/infinityi/inventorymenu/InventoryMenu.java
@@ -3,10 +3,8 @@ package infinityi.inventorymenu;
 import infinityi.inventorymenu.command.MenuCommand;
 import infinityi.inventorymenu.dataparser.ConfigManager;
 import infinityi.inventorymenu.dataparser.DataManager;
+import infinityi.inventorymenu.event.UseItemEvent;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
-import net.minecraft.resource.ResourceType;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,21 +12,15 @@ public class InventoryMenu implements ModInitializer {
 
     public static final String MOD_ID = "inventory-menu";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-    private static DataManager dataManager;
-
-    public static DataManager getDataManager() {
-        return dataManager;
-    }
+    public static DataManager dataManager;
 
     @Override
     public void onInitialize() {
         ConfigManager.loadConfig();
         MenuCommand.register();
+        UseItemEvent.register();
         dataManager = new DataManager();
-
-        ResourceLoader.get(ResourceType.SERVER_DATA).registerReloader(Identifier.of(InventoryMenu.MOD_ID, "menu_data_manager"), dataManager.menus());
-        ResourceLoader.get(ResourceType.SERVER_DATA).registerReloader(Identifier.of(InventoryMenu.MOD_ID, "item_menu_data_manager"), dataManager.items());
+        dataManager.register();
         ConfigManager.saveConfig();
     }
 }
