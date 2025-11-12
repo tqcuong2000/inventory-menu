d0c83a9fae816bbb3b52f75c070dab9baedafa36
cuong <cuong26092@gmail.com>
2025-11-12 08:33:22 +0700

    display an error item instead of empty slot if an defined item is not found

--- src/main/java/infinityi/inventorymenu/menu/itemtype/DefinedItem.java
+++ src/main/java/infinityi/inventorymenu/menu/itemtype/DefinedItem.java
@@ -6,9 +6,14 @@ import infinityi.inventorymenu.InventoryMenu;
 import infinityi.inventorymenu.action.Action;
 import infinityi.inventorymenu.menu.layout.MenuItem;
 import infinityi.inventorymenu.menu.layout.MenuItemType;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,8 +28,8 @@ public record DefinedItem(Identifier id) implements MenuItem {
     }
 
     @Override
-    public ItemStack resolveItemStack(ServerPlayerEntity player) {
-        return getItem().map(item -> item.resolveItemStack(player)).orElse(ItemStack.EMPTY);
+    public @NotNull ItemStack resolveItemStack(ServerPlayerEntity player) {
+        return getItem().map(item -> item.resolveItemStack(player)).orElse(getErrorItem());
     }
 
     @Override
@@ -34,5 +39,13 @@ public record DefinedItem(Identifier id) implements MenuItem {
 
     private Optional<MenuItem> getItem(){
         return InventoryMenu.dataManager.items().getItem(id);
+    }
+
+    private ItemStack getErrorItem(){
+        ItemStack errorItem = Items.ARROW.getDefaultStack();
+        Text name = Text.literal("§cDefined item §7%s §cnot found.".formatted(id.toString()));
+        errorItem.set(DataComponentTypes.CUSTOM_NAME, name);
+        errorItem.set(DataComponentTypes.ITEM_MODEL, Registries.ITEM.getId(Items.BARRIER));
+        return errorItem;
     }
 }
