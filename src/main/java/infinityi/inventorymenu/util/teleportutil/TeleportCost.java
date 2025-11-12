78fba689e0b47d3d2509d6adcbfaf29eadc26ced
cuong <cuong26092@gmail.com>
2025-11-12 08:34:32 +0700

    fixed teleport action still allow player to teleport even their xp level isn't enough

--- src/main/java/infinityi/inventorymenu/util/teleportutil/TeleportCost.java
+++ src/main/java/infinityi/inventorymenu/util/teleportutil/TeleportCost.java
@@ -41,8 +41,7 @@ public record TeleportCost(List<Integer> amount, Integer saturation, Boolean isP
     }
 
     public boolean hasCost(ServerPlayerEntity player, BlockPos pos) {
-        if (amount.getFirst().equals(0)) return true;
-        return player.experienceLevel > calcCost(player, pos);
+        return player.experienceLevel >= calcCost(player, pos);
     }
 
     public void applyCost(ServerPlayerEntity player, BlockPos pos) {
